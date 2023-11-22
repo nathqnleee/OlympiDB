afe9f7fdd1bbe91610af2709d8335487f4266ae1
Nathan Lee <nsjlee33@hotmail.com>
2023-11-21 22:51:10 -0800

    Created Page for Inserting, Updating and Deleting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,54 @@
 import logo from './logo.svg';
+import Olympics from './olympics.png';
+import Animals from './animals.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
-    </div>
+      <img src={Olympics} alt="Olympics Logo" />  
+      <h1>Insert Athlete</h1>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="First Name" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="Last Name" />
+          </label>
+          <label>
+            Gender:
+            <input type="text" name="Gender" />
+          </label>
+          <input type="submit" value="Insert" />
+        </form>
+      <h1>Update Athlete</h1>
+      <form>
+          <label>
+            First Name:
+            <input type="text" name="First Name" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="Last Name" />
+          </label>
+          <label>
+            For:
+            <input type="text" name="PlayerID" />
+          </label>
+          <input type="submit" value="Update" />
+        </form>
+      <h1>Delete Athlete Using PlayerID</h1>
+        <form>
+            <label>
+              PlayerID:
+              <input type="text" name="PlayerID" />
+            </label>
+            <input type="submit" value="Delete" />
+          </form>
+      <img src={Animals} alt="Animals Photo" />   
+      </div>
   );
 }
 

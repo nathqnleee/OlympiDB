3cbfcc5712d4739fbd41c4e3d631bf4330c74caa
Nathan Lee <nsjlee33@hotmail.com>
2023-11-26 19:22:24 -0800

    created simple front page

---
+++ frontend/src/screens/frontPage.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Olympics from '../olympics.png';
+import {Link} from 'react-router-dom';
+
+function frontPage() {
+    return (
+        <div>
+            <img src={Olympics} alt="Olympics Logo" />
+          <header>
+            <h1>Olympics Database Application</h1>
+          </header>
+          <Link to ="/searchDatabase">Search Database    </Link>
+          
+          <Link to ="/manageDatabase">Manage Database</Link>
+        </div>
+      );
+}
+  
+  export default frontPage;

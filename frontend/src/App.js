23c43f9aa4111dd845de4ba0291574556526c194
jjessieshang <17jessies@gmail.com>
2023-11-27 19:08:53 -0800

    login verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ManageDatabase from "./screens/manageDatabase";
 import SearchDatabase from "./screens/searchDatabase";
+import LoginPage from "./screens/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ManageDatabase />} />
+        <Route path="/login" element={<LoginPage />} />
+          <Route path="/manageDatabase" element={<ManageDatabase />} />
           <Route path="/searchDatabase" element={<SearchDatabase />} />
         </Routes>
       </BrowserRouter>
